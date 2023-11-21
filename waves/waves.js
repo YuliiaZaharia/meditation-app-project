215ad4e093c54154257388925f0dc7466d31cacf
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-21 12:16:11 +0300

    applied same methods for nature.js and rain.js

--- waves/waves.js
+++ waves/waves.js
@@ -9,17 +9,21 @@ function calculateTime () {
     let minutes = Math.floor(amountTime/60); 
     let seconds = amountTime%60; 
 
+    if (seconds < 10) {
+    seconds = "0" + seconds; 
+    }
+
     countdown.textContent = `${minutes} : ${seconds}`;
     amountTime--;
 
-    if (amountTime < 0); 
+    if (amountTime < 0) {
     stopTimer();
     amountTime = 0; 
+    }
 
     function stopTimer () {
     clearInterval(timeId); 
     }
-
 }
 
 let timeId = setInterval(calculateTime, 1000); 
