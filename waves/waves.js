9365172d542e411b06d3ec1573bbc8f45ceddeef
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-21 12:59:32 +0300

    adaptation to tablet devices

--- waves/waves.js
+++ waves/waves.js
@@ -1,8 +1,8 @@
 const button = document.querySelector("#myButton");
 
 button.addEventListener("click", function() {
-document.querySelector("#player").play; 
-document.querySelector("#myVideo").play; 
+document.querySelector("#player").play(); 
+document.querySelector("#myVideo").play(); 
 })
 
 // minutes 
